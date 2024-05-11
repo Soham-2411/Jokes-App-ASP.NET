61be7117e69d84b34d9257aaa7f0f5b6a2f4c3bf
Soham Sakaria <soham.s2411@gmail.com>
2024-05-11 13:10:20 -0400

    Project upload

---
+++ Models/Jokes.cs
@@ -0,0 +1,14 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set;}
+
+        public Jokes()
+        {
+            
+        }
+    }
+}
